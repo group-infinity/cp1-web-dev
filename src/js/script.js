b800273fbb9a7649b3d2e1a45dba1dfcc7015c87
Miguel <miguel.amarod37@gmail.com>
2025-04-14 11:23:52 -0300

    Adicionando Questão 3

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,34 @@
+// Questão 3 - Miguel
+console.log("QUESTÃO 3")
+let peso = 60;
+let altura = 1.80;
+const imc = (peso / (altura * altura));
+
+switch (true) {
+    case (imc < 18.5):
+        console.log(`Seu IMC é de ${imc.toFixed(2)}, Você está abaixo do peso.`);
+        break;
+    case (imc <= 24.9):
+        console.log(`Seu IMC é de ${imc.toFixed(2)}, Você está no peso ideal.`);
+        break;
+    case (imc >= 25):
+        console.log(`Seu IMC é de ${imc.toFixed(2)}, Você está acima do peso.`);
+        break;
+    default:
+        console.log("Valor inválido");
+}
+
+console.log("----------------------------------------------------");
+
 //Questão 4 - Diogo
+console.log("QUESTÃO 4")
 for(let i=1;i<=50;i++){
-    console.log('!', i); }
+    console.log('O valor é:', i); }
 
-
+console.log("----------------------------------------------------");
 
 //Questão 9 - Diogo
+console.log("QUESTÃO 9")
 let string = "123.456";
 let valor = parseFloat(string);
 console.log(valor);
